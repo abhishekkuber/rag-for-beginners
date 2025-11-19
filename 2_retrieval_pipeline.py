32c30bd1ca7526291be36a0e5f3a17785ce4c892
abhishekkuber <abhi.kuber@gmail.com>
2025-11-19 11:40:39 +0100

    add basic generation

--- 2_retrieval_pipeline.py
+++ 2_retrieval_pipeline.py
@@ -1,6 +1,17 @@
+# Code which retrieves the relevant documents from the vector database 
+# and then uses a LLM to answer the question.
+# Please note that this is a one off generation (this means that this is not a conversational RAG)
+
+import os
+
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
 from dotenv import load_dotenv
+
+from langchain_huggingface import ChatHuggingFace
+from langchain_huggingface.llms import HuggingFaceEndpoint
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
@@ -41,3 +52,39 @@ relevant_docs = retriever.invoke(query)
 #     print(f"Document {i}\n{doc.page_content}")
 #     print("-"*100)
 
+
+
+############# Create a generation model (LLM) #############
+combined_input = f"""Based on the following documents, please answer this question : {query}
+
+Documents : 
+{chr(10).join([f" - {doc.page_content}" for doc in relevant_docs])}
+
+Please provide a clear, helpful answer using only the information from these documents. If you can't find the answer in the documents, say "I don't have enough information to answer the question".
+"""
+
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+
+llm = HuggingFaceEndpoint(
+    model="meta-llama/Llama-3.1-8B-Instruct",
+    task="text-generation",
+    max_new_tokens=200,
+    temperature=0.7,
+    provider="auto"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+# messages = ChatPromptTemplate.from_messages([
+#     ("system", "You are a helpful assistant which answers questions based on the provided documents."),
+#     ("user", combined_input)
+# ])
+
+messages = [
+    SystemMessage(content="You are a helpful assistant that answers questions based on the provided documents. If you can't find the answer in the "),
+    HumanMessage(content=combined_input)
+]
+
+response = model.invoke(messages)
+
+print(response.content)
